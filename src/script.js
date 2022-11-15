c8e8bef472e26e0cf3869fbd7d818f7b737fffdf
apaulineoliveira <apaulineoliveira@gmail.com>
2022-11-14 23:58:22 -0300

    update

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("filter-item")) {
+        filterContainer.querySelector(".active").classList.remove("active");
+
+        event.target.classList.add("active");
+        const filterValue = event.target.getAttribute("data-filter");
+        galleryItems.forEach((item) => {
+            if(item.classList.contains(filterValue) || filterValue === 'all'){
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
